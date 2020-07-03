e03c52a20f1b9d7edf12240176b032e309085d7a
luan.lazzari <luanlazzari@gmail.com>
2020-07-03 17:04:29 -0300

    test: ensure RequiredFieldValidation not return if validation success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,9 +2,15 @@ import { RequireFieldValidation } from './require-field-validation'
 import { MissingParamError } from '../../errors'
 
 describe('RequireField Validation', () => {
-  test('should Should return a MissingParamError if validation fails', () => {
+  test('Should return a MissingParamError if validation fails', () => {
     const sut = new RequireFieldValidation('field')
     const error = sut.validate({ name: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation success', () => {
+    const sut = new RequireFieldValidation('field')
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
   })
 })
