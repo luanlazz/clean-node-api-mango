d28da0fa939dbb5b3eff46556c98b8ccafb05a02
luan.lazzari <luanlazzari@hotmail.com>
2020-07-09 00:08:11 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,6 +1,6 @@
-import { ValidationComposite, RequireFieldValidation, EmailValidation } from '../../../presentation/helpers/validators'
-import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
+import { ValidationComposite, RequireFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/main/factories/controllers/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { ValidationComposite, RequireFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../presentation/helpers/validators'
+import { ValidationComposite, RequireFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../../presentation/helpers/validators'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidator } from '../../../../presentation/protocols/email-validator'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite.ts')
+jest.mock('../../../../presentation/helpers/validators/validation-composite.ts')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
