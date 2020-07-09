ae8ec6878a5f17f8af40fb660f68391276a6a6b9
luan.lazzari <luanlazzari@hotmail.com>
2020-07-09 15:22:00 -0300

    refactor: change files folder (tests)

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { ValidationComposite, RequireFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators'
+import { ValidationComposite, RequireFieldValidation, EmailValidation } from '../../../../validation/validators'
 import { makeLoginValidation } from './login-validation-factory'
 import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { EmailValidator } from '../../../../validation/protocols/email-validator'
 
-jest.mock('../../../../presentation/helpers/validators/validation-composite.ts')
+jest.mock('../../../../validation/validators/validation-composite.ts')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
---
+++ src/validation/validators/required-field-validation.spec.ts
@@ -0,0 +1,20 @@
+import { RequireFieldValidation } from './require-field-validation'
+import { MissingParamError } from '../../presentation/errors'
+
+const makeSut = (): RequireFieldValidation => {
+  return new RequireFieldValidation('field')
+}
+
+describe('RequireField Validation', () => {
+  test('Should return a MissingParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ name: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation success', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
