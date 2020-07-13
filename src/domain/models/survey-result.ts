8caa96b6c6d31b7d1782a8f6cefeda7025a44b44
luan.lazzari <luanlazzari@hotmail.com>
2020-07-13 19:21:06 -0300

    feat: add survey result model

---
+++ src/domain/models/survey-result.ts
@@ -0,0 +1,7 @@
+export type SurveyResultModel = {
+  id: string
+  accountId: string
+  surveyId: string
+  answer: string
+  date: Date
+}
--- src/domain/models/survey.ts
+++ src/domain/models/survey.ts
@@ -9,11 +9,3 @@ export type SurveyAnswerModel = {
   image?: string
   answer: string
 }
-
-export type SurveyResultModel = {
-  id: string
-  accountId: string
-  surveyId: string
-  answer: string
-  date: Date
-}
