2b563a14e841617ca28844a46fcb18808bf1718f
luan.lazzari <luanlazzari@hotmail.com>
2020-07-15 20:30:23 -0300

    feat: add Swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API of Mango course to make surveys with programmers',
+    version: '1.0.0'
+  }
+}
