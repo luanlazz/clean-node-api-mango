de2ee1651e91943fb41f77e8154a95aee8ad038b
luan.lazzari <luanlazzari@hotmail.com>
2020-07-17 21:17:02 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyByIdRepo if LoadSurveyResultRepo return null

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -1,5 +1,5 @@
 export * from '@/data/protocols/db/survey-result/load-survey-result-repository'
+export * from '@/data/protocols/db/survey/load-survey-by-id-repository'
 export * from '@/data/test/mock-db-survey-result'
-export * from '@/data/protocols/db/survey-result/load-survey-result-repository'
 export * from '@/domain/usecases/survey-result/load-survey-resultts'
 export * from '@/domain/models/survey-result'
