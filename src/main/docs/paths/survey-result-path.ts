0e0f112e557ff4397b929f189c66ab15e69cee24
luan.lazzari <luanlazzari@hotmail.com>
2020-07-18 01:47:59 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,6 +5,7 @@ export const surveyResultPath = {
     }],
     tags: ['Survey result'],
     summary: 'API to create an answer of a survey',
+    description: 'This route just can be access by authenticate user',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -22,6 +23,43 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey result'],
+    summary: 'API to load answers of a survey',
+    description: 'This route just can be access by authenticate user',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Success',
